2c77402787c0761da4e0fa8f500bba27857d281f
Soham Solanki <sohammsolanki@gmail.com>
2024-07-05 23:27:06 +0530

    frontend error solved!

--- frontend/src/components/Header/Dashboard.jsx
+++ frontend/src/components/Header/Dashboard.jsx
@@ -1,64 +1,107 @@
-import React, { useContext } from 'react';
-import { UserContext } from "../../context/userContext";
-import LSPDLogo from '/src/assets/lspd-logo.png';
+import React, {useContext} from 'react'
+import {UserContext} from '../../context/userContext'
+import LSPDLogo from '/src/assets/lspd-logo.png'
+import Starter from '../Starter'
+// import Dashboard from './components/Header/Dashboard.jsx'
+import WantedList from './../WantedList'
 
 const dashboardData = [
     {
-        image: "/src/assets/wanted.png",
-        title: "Most Wanted List",
-        description: "Catch Them if You Can (But Maybe Don’t Try Too Hard)"
+        image: '/src/assets/wanted.png',
+        title: 'Most Wanted List',
+        description: 'Catch Them if You Can (But Maybe Don’t Try Too Hard)',
     },
     {
-        image: "/src/assets/tip.png",
-        title: "Submit a Tip",
-        description: "Got a Hot Tip? Let Us Know (or Just Gossip, We’re Not Judging)"
+        image: '/src/assets/tip.png',
+        title: 'Submit a Tip',
+        description:
+            'Got a Hot Tip? Let Us Know (or Just Gossip, We’re Not Judging)',
     },
     {
-        image: "/src/assets/careers.png",
-        title: "Careers at LSPD",
-        description: "From Rookie to All-Star Cop – Start Your Journey in Los Santos"
+        image: '/src/assets/careers.png',
+        title: 'Careers at LSPD',
+        description:
+            'From Rookie to All-Star Cop – Start Your Journey in Los Santos',
     },
     {
-        image: "/src/assets/news.png",
-        title: "News and Alerts",
-        description: "Stay Informed on the Latest in Los Santos"
-    }
-];
+        image: '/src/assets/news.png',
+        title: 'News and Alerts',
+        description: 'Stay Informed on the Latest in Los Santos',
+    },
+]
 
 const Dashboard = () => {
-    const { user } = useContext(UserContext);
+    const {user} = useContext(UserContext)
 
     return (
         <div className="bg-[var(--bg1)] text-[var(--lblue)] min-h-screen">
+            <Starter />
             <section className="p-12">
                 <div className="text-center m-8 p-12">
-                    <h2 className="text-5xl pricedown font-bold text-[var(--lblue)] mb-4">Welcome to LSPD Eagle-eye</h2>
-                    <p className="text-2xl poppins mb-2 text-[var(--ltext)]">Your Digital Hotline for All Things Los Santos! (Yes, Even the Crazy Stuff)</p>
+                    <h2 className="text-5xl pricedown font-bold text-[var(--lblue)] mb-4">
+                        Welcome to LSPD Eagle-eye
+                    </h2>
+                    <p className="text-2xl poppins mb-2 text-[var(--ltext)]">
+                        Your Digital Hotline for All Things Los Santos! (Yes,
+                        Even the Crazy Stuff)
+                    </p>
                 </div>
                 <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-10 pr-4 md:px-20">
                     {dashboardData.map((item, index) => (
-                        <div key={index} className="flex bg-[var(--bg1l)] p-3 rounded-2xl shadow-2xl text-left items-center transform transition-transform duration-300 hover:scale-105 hover:border-[var(--hover-border-color)] hover:border-2 hover:bg-gradient-to-r from-[var(--hover-bg-gradient-start)] to-[var(--hover-bg-gradient-end)]">
-                            <img src={item.image} alt={`Image ${index + 1}`} className="w-[30%] rounded-lg mr-6" />
+                        <div
+                            key={index}
+                            className="flex bg-[var(--bg1l)] p-3 rounded-2xl shadow-2xl text-left items-center transform transition-transform duration-300 hover:scale-105 hover:border-[var(--hover-border-color)] hover:border-2 hover:bg-gradient-to-r from-[var(--hover-bg-gradient-start)] to-[var(--hover-bg-gradient-end)]"
+                        >
+                            <img
+                                src={item.image}
+                                alt={`Image ${index + 1}`}
+                                className="w-[30%] rounded-lg mr-6"
+                            />
                             <div>
-                                <h3 className={`pricedown text-4xl mb-2 ${item.title === "Most Wanted List" || item.title === "News and Alerts" ? "text-[var(--lgold)]" : ""}`}>
+                                <h3
+                                    className={`pricedown text-4xl mb-2 ${item.title === 'Most Wanted List' || item.title === 'News and Alerts' ? 'text-[var(--lgold)]' : ''}`}
+                                >
                                     {item.title}
                                 </h3>
-                                <p className="text-[var(--ltext)] text-xl">{item.description}</p>
+                                <p className="text-[var(--ltext)] text-xl">
+                                    {item.description}
+                                </p>
                             </div>
                         </div>
                     ))}
                 </div>
                 <div className="bg-[var(--bg1l)] p-12 rounded-2xl shadow-2xl text-left mt-10 mb-10 mx-4 md:mx-20">
-                    <h3 className="text-4xl pricedown text-[var(--lgold)] font-bold">Why LSPD Eagle-eye?</h3>
+                    <h3 className="text-4xl pricedown text-[var(--lgold)] font-bold">
+                        Why LSPD Eagle-eye?
+                    </h3>
                     <p className="my-8 text-[var(--ltext)] text-xl">
-                        The LSPD Eagle-eye portal was created to connect the Los Santos Police Department with its vibrant community. In a dynamic and unpredictable city like Los Santos, staying ahead requires more than just police presence; it needs the vigilance of every citizen. This user-friendly portal allows residents to report crimes, share tips, and stay informed about local safety issues. By harnessing community input, LSPD Eagle-eye aims to enhance public safety, build trust, and ensure a responsive and transparent police force. Whether it’s suspicious activity or crucial information, your input can make a difference.
+                        The LSPD Eagle-eye portal was created to connect the Los
+                        Santos Police Department with its vibrant community. In
+                        a dynamic and unpredictable city like Los Santos,
+                        staying ahead requires more than just police presence;
+                        it needs the vigilance of every citizen. This
+                        user-friendly portal allows residents to report crimes,
+                        share tips, and stay informed about local safety issues.
+                        By harnessing community input, LSPD Eagle-eye aims to
+                        enhance public safety, build trust, and ensure a
+                        responsive and transparent police force. Whether it’s
+                        suspicious activity or crucial information, your input
+                        can make a difference.
                     </p>
-                    <p className="mb-8 text-[var(--ltext)] text-xl">Join us in keeping Los Santos safe and lively, one tip at a time!</p>
-                    <img src="/src/assets/place.png" alt="Place" className="rounded-xl shadow-xl w-[100%]" />
+                    <p className="mb-8 text-[var(--ltext)] text-xl">
+                        Join us in keeping Los Santos safe and lively, one tip
+                        at a time!
+                    </p>
+                    <img
+                        src="/src/assets/place.png"
+                        alt="Place"
+                        className="rounded-xl shadow-xl w-[100%]"
+                    />
                 </div>
             </section>
+            <WantedList />
         </div>
-    );
-};
+    )
+}
 
-export default Dashboard;
+export default Dashboard
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,28 +13,23 @@ import {
 import Login from './components/Header/Login.jsx'
 import Register from './components/Header/Register.jsx'
 import Dashboard from './components/Header/Dashboard.jsx'
-
+import Starter from './components/Starter'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
+            <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />,
             <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
         </Route>,
     ),
 )
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RouterProvider router={router} />
     </React.StrictMode>,
 )
-
-
-
-
 
 // this is for the learning perpose
 // const router = createBrowserRouter([
