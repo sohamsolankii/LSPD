9ac60c84885c0bea5785ae939d0b7d052d3c0d51
Meet Suthar <meetsuthar2005@gmail.com>
2024-07-21 00:17:07 +0530

    Improved likes and comments in news

--- frontend/src/components/Pages/news/News.jsx
+++ frontend/src/components/Pages/news/News.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {FaThumbsUp, FaCommentAlt} from 'react-icons/fa'
 
 const newsArticles = [
     {
@@ -78,14 +79,14 @@ const News = () => {
     }
 
     return (
-        <div className="w-full bg-[var(--bg1)] dark:bg-[var(--dbg1)] mx-auto p-4 md:p-10">
+        <div className="w-full dark:bg-gray-100 bg-[var(--bg2)] mx-auto p-4 md:p-10 min-h-screen">
             {selectedArticle ? (
                 <div>
-                    <div className="max-w-[100%] mx-auto bg-[var(--bg1l)]  mt-20 dark:bg-[var(--dbg1l)] text-[var(--lgold)] dark:text-[var(--dltext)] shadow-lg dark:shadow-sm rounded-lg md:rounded-2xl overflow-hidden">
+                    <div className="max-w-[100%] mx-auto text-[var(--lgold)] dark:text-[var(--dltext)] overflow-hidden rounded-3xl shadow-black/70  dark:shadow-black/10 bg-[var(--bg1)] dark:bg-gray-100 dark:border-gray-400 border-[1px] border-[var(--opac)] shadow-2xl">
                         <div className="block md:flex p-3">
                             <button
                                 onClick={handleBack}
-                                className="m-3 bg-blue-500 poppins text-white px-4 h-10 rounded-md"
+                                className="p-2 px-5 m-3 poppins border-[1px] border-[var(--opac)] rounded-md bg-[var(--bg4op)] dark:bg-[var(--bg1l)] backdrop-blur-md shadow-black/30 hover:shadow-black/40 hover:bg-[var(--opac)] hover:dark:bg-[var(--bg1)] shadow-md hover:shadow-xl text-gray-200 tems-center justify-center"
                             >
                                 Back to News
                             </button>
@@ -93,25 +94,28 @@ const News = () => {
                                 {selectedArticle.headline}
                             </h2>
                         </div>
-                        <img
-                            src={selectedArticle.image}
-                            alt={selectedArticle.headline}
-                            className="w-full rounded-xl p-4 object-cover"
-                        />
-                        <div className="p-4 poppins text-sm md:text-lg text-[var(--ltext)] dark:text-[var(--dlblue)]">
-                            <p>{selectedArticle.content}</p>
+                        <div className="px-2">
+                            <img
+                                src={selectedArticle.image}
+                                alt={selectedArticle.headline}
+                                className="md:w-[30%] md:p-0 p-4 w-[100%] rounded-lg md:m-5 m-0 float-none md:float-left"
+                            />
+                            <div className="p-4 poppins text-justify font-light text-sm md:text-lg text-gray-400 dark:text-[var(--dlblue)]">
+                                <p>{selectedArticle.content}</p>
+                            </div>
                         </div>
-                        <div className="p-4 poppins text-sm">
-                            <div className="md:flex block justify-center">
+                        <div className="m-5 poppins text-sm max-w-[100%] text-[var(--lgold)] dark:text-[var(--dltext)] rounded-2xl shadow-black/70  dark:shadow-black/10 bg-[var(--bg1)] dark:bg-gray-100 dark:border-gray-400 border-[1px] border-[var(--opac)] shadow-2xl">
+                            <div className="md:flex m-5 block justify-center items-center">
                                 <button
                                     onClick={() =>
                                         handleLike(selectedArticle.id)
                                     }
-                                    className="bg-blue-500 text-white h-9 px-2 py-1 mr-5 mb-2 md:mb-4 md:mr-20 rounded-md"
+                                    className="bg-[var(--opac)] mr-10 mb-3 md:mb-0 text-white h-9 px-4 py-1 rounded-full flex items-center shadow-md hover:bg-blue-600 dark:bg-blue-400 hover:dark:bg-blue-800 transition duration-300 shadow-black/70 shadow-lg dark:shadow-black/20 border-y-[1px] border-[var(--opac2)] dark:border-blue-800"
                                 >
-                                    Like ({selectedArticle.likes})
+                                    <FaThumbsUp className="mr-2" />
+                                    {selectedArticle.likes}
                                 </button>
-                                <div>
+                                <div className="items-center inline-flex">
                                     <input
                                         type="text"
                                         value={comment}
@@ -119,24 +123,25 @@ const News = () => {
                                             setComment(e.target.value)
                                         }
                                         placeholder="Add a comment"
-                                        className="border-2 border-[var(--dbg2)] dark:border-[var(--dlgold)] rounded-md bg-[var(--ltext)] text-[var(--bg1l)] px-2 py-1 h-9 mr-2"
+                                        className="rounded-l-full bg-[var(--opac2)] dark:bg-[var(--ltext)] dark:text-[var(--bg1l)] text-gray-300 px-5 py-1 h-9 shadow-black/70 shadow-lg dark:shadow-black/20 border-y-[1px] border-[var(--opac2)] dark:border-blue-800"
                                     />
                                     <button
                                         onClick={() =>
                                             handleComment(selectedArticle.id)
                                         }
-                                        className="bg-blue-500 text-white h-9 px-2 py-1 rounded-md"
+                                        className="bg-[var(--opac)] text-white h-9 px-4 py-1 rounded-r-full flex items-center shadow-md hover:bg-blue-600 dark:bg-blue-400 hover:dark:bg-blue-800 transition duration-300 shadow-black/70 shadow-lg dark:shadow-black/20 border-y-[1px] border-[var(--opac2)] dark:border-blue-800"
                                     >
+                                        <FaCommentAlt className="mr-2" />
                                         Comment
                                     </button>
                                 </div>
                             </div>
-                            <div className="mt-4">
+                            <div className="mt-4 p-4">
                                 {selectedArticle.comments.map(
                                     (comment, index) => (
                                         <div
                                             key={index}
-                                            className="border-b py-2"
+                                            className="border-b-[1px] border-[var(--opac)] dark:border-gray-400 py-2"
                                         >
                                             {comment}
                                         </div>
@@ -147,7 +152,7 @@ const News = () => {
                     </div>
                 </div>
             ) : (
-                <div classname=" mt-20">
+                <div className="min-h-screen">
                     <div className="md:p-6 md:m-6 p-2 m-2 mt-6 align-center text-center justify-center">
                         <h2 className="text-xl md:text-4xl pricedown mb-4 text-[var(--lgold)] dark:text-[var(--dlgold)]">
                             News and Alerts
@@ -156,27 +161,34 @@ const News = () => {
                             Stay Updated with all news of Los Santos!
                         </p>
                     </div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-8">
+                    <div className="grid grid-cols-1 md:grid-cols-2 sm:p-4 p-3 lg:grid-cols-4 gap-3 rounded-3xl shadow-black/70  dark:shadow-black/10 bg-[var(--bg1)] dark:bg-gray-100 dark:border-gray-400 border-[1px] border-[var(--opac)] shadow-2xl md:gap-4">
                         {articles.map((article) => (
                             <div
                                 key={article.id}
-                                className="bg-[var(--bg1l)] dark:bg-[var(--dbg1l)] hover:bg-[var(--hover-bg-gradient-end)] hover:dark:bg-[var(--dllgold)] text-[var(--ltext)] dark:text-[var(--dlblue)] hover:text-[var(--lgold)] hover:dark:text-[var(--dltext)] shadow-lg dark:shadow-sm rounded-lg md:rounded-2xl overflow-hidden cursor-pointer p-3 md:p-4 transition ease-in-out duration-1500"
+                                className="text-[var(--ltext)] dark:text-[var(--dlblue)] hover:text-[var(--lgold)] hover:dark:text-[var(--dltext)] sm:p-4 p-2 hover:dark:bg-white hover:bg-[var(--opac2)] bg-[var(--opac)] dark:bg-gray-100 border-[1px] border-[var(--opac)] dark:border-gray-300 rounded-2xl shadow-black/30 dark:shadow-none overflow-hidden cursor-pointer p-2 md:p-3 transition ease-in-out duration-1500"
                                 onClick={() => handleClick(article)}
                             >
                                 <img
                                     src={article.image}
                                     alt={article.headline}
-                                    className="w-full md:h-52 h-30 md:rounded-md rounded-sm shadow-sm object-cover"
+                                    className="w-full md:h-52 h-25 md:rounded-md rounded-md shadow-sm object-cover"
                                 />
-                                <div className="md:p-4 p-0 pt-3">
+                                <div className="md:p-4 p-0 pt-2">
                                     <h3 className="md:text-lg text-sm poppins font-medium">
                                         {article.headline}
                                     </h3>
-                                    <div className="flex justify-between mt-2">
-                                        <span>Likes: {article.likes}</span>
-                                        <span>
-                                            Comments: {article.comments.length}
-                                        </span>
+                                    <div className="flex mt-2 border-[1px] justify-center text-xs border-[var(--opac)] dark:border-gray-300px-4 py-2 rounded-full items-center">
+                                        <div className="flex items-center">
+                                            <FaThumbsUp />
+                                            <span>{article.likes}</span>
+                                        </div>
+                                        <div className="border-l h-4 border-gray-400 mx-3"></div>
+                                        <div className="flex items-center">
+                                            <FaCommentAlt />
+                                            <span>
+                                                {article.comments.length}
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
