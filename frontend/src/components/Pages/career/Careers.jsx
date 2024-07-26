accf6da7f5d936ddd748570cee59c9054f5d18cc
Meet Suthar <meetsuthar2005@gmail.com>
2024-07-26 18:00:40 +0530

    Careers done

--- frontend/src/components/Pages/career/Careers.jsx
+++ frontend/src/components/Pages/career/Careers.jsx
@@ -7,17 +7,10 @@ import {FaLocationDot} from 'react-icons/fa6'
 import toast from 'react-hot-toast'
 import {useNavigate} from 'react-router-dom'
 
-
 const Careers = () => {
     const [jobs, setJobs] = useState([])
     const navigate = useNavigate()
 
-    const [filters, setFilters] = useState({
-        salaryRange: '',
-        department: '',
-        location: '',
-    })
-    const [search, setSearch] = useState('')
     const [selectedJob, setSelectedJob] = useState(null)
 
     useEffect(() => {
@@ -32,10 +25,8 @@ const Careers = () => {
             if (err.response && err.response.status === 404) {
                 toast.error('This job has already been applied')
             } else {
-                toast.error(
-                    'You must have to login first!',
-                )
-				navigate('/login')
+                toast.error('You must have to login first!')
+                navigate('/login')
             }
             console.log(err)
         }
@@ -56,7 +47,7 @@ const Careers = () => {
     return (
         <div className="p-4 md:p-6 min-h-screen poppins bg-cover dark:bg-gray-100 bg-[var(--bg2)]">
             <div className="mb-6 rounded-2xl shadow-black/70 dark:shadow-black/10 bg-[var(--bg1)] dark:bg-gray-100 dark:border-gray-400 border-[1px] border-[var(--opac)] ">
-                <h2 className="text-2xl rounded-xl font-medium m-2 p-2 text-[var(--lgold)] dark:text-[var(--dltext)] text-center dark:shadow-black/10 shadow-black/70 bg-[var(--bg1)] dark:bg-gray-100 dark:border-gray-400">
+                <h2 className="text-3xl rounded-xl font-medium m-2 p-2 text-[var(--lgold)] dark:text-[var(--dltext)] text-center dark:shadow-black/10 shadow-black/70 bg-[var(--bg1)] dark:bg-gray-100 dark:border-gray-400">
                     Careers
                 </h2>
 
@@ -132,7 +123,7 @@ const Careers = () => {
 
                     <div className="inline-flex items-center mb-[50px] justify-center w-full">
                         <hr className="w-[90%] h-px my-8 bg-[var(--lgold)] border-0 dark:bg-[var(--dltext)]"></hr>
-                        <span className="absolute px-3 text-4xl text-[var(--lgold)] -translate-x-1/2 bg-[var(--bg1)] left-1/2 dark:text-[var(--dltext)] dark:bg-gray-100">
+                        <span className="absolute px-3 text-lg text-[var(--lgold)] -translate-x-1/2 bg-[var(--bg1)] left-1/2 dark:text-[var(--dltext)] dark:bg-gray-100">
                             Explore Careers
                         </span>
                     </div>
@@ -141,46 +132,39 @@ const Careers = () => {
                         {jobs.map((job) => (
                             <div
                                 key={job._id}
-                                className="p-4 bg-[var(--opac)] h-[300px] dark:bg-gray-100 border-[1px] border-[var(--opac)] dark:border-gray-300 rounded-md shadow-black/30 dark:shadow-none shadow-md"
+                                className="p-6 bg-[var(--opac)] dark:bg-gray-100 border-[1px] border-[var(--opac)] dark:border-gray-300 rounded-lg shadow-black/50 shadow-lg dark:shadow-none hover:bg-[var(--opac2)] hover:dark:bg-blue-100"
                             >
-                                <div className="flex flex-col h-full justify-evenly">
-                                    <div className="flex items-center mb-2">
-                                        <span className="text-gray-400 dark:text-gray-500 font-medium text-2xl mr-1">
-                                            Job Title:
-                                        </span>
-                                        <h3 className="text-2xl md:text-2xl font-medium text-gray-200 dark:text-[var(--dltext)]">
-                                            {job.title}
-                                        </h3>
-                                    </div>
-                                    <div className="flex items-center text-md text-gray-200 dark:text-gray-600 mb-2">
-                                        <span className="text-gray-400 dark:text-gray-500 font-medium mr-1">
-                                            Department:
-                                        </span>
-                                        <span>{job.department || 'LSPD'}</span>
-                                    </div>
-                                    <div className="text-md text-gray-200 dark:text-gray-600 mb-2">
-                                        <span className="text-gray-400 dark:text-gray-500 font-medium mr-1">
-                                            Location:
-                                        </span>
-                                        <span>{job.location}</span>
-                                    </div>
-                                    <div className="text-md text-gray-200 dark:text-gray-600 mb-2">
-                                        <span className="text-gray-400 dark:text-gray-500 font-medium mr-1">
-                                            Type:
-                                        </span>
-                                        <span>{job.jobType}</span>
-                                    </div>
-                                    <div className="text-md text-gray-200 dark:text-gray-600 mb-4">
-                                        <span className="text-gray-400 dark:text-gray-500 font-medium mr-1">
-                                            Salary:
-                                        </span>
-                                        <span>${job.salaryRange}</span>
+                                <div className="flex flex-col h-full justify-between">
+                                    <div className="flex flex-col gap-1 mb-4">
+                                        <div className="flex items-center mb-2">
+                                            <h3 className="text-xl font-semibold text-[var(--lgold)] dark:text-[var(--dltext)]">
+                                                {job.title}
+                                            </h3>
+                                        </div>
+                                        <hr className="h-px mb-2 bg-[var(--opac2)] border-0 dark:bg-gray-300"></hr>
+                                        <div className="flex items-center gap-2 text-md text-gray-200 dark:text-gray-600">
+                                            <AiIcons.AiOutlineUser />
+                                            <span>
+                                                {job.department || 'LSPD'}
+                                            </span>
+                                        </div>
+                                        <div className="flex items-center gap-2 text-md text-gray-200 dark:text-gray-600">
+                                            <FaLocationDot />
+                                            <span>{job.location}</span>
+                                        </div>
+
+                                        <div className="flex items-center gap-2 text-md text-gray-200 dark:text-gray-600">
+                                            <div>
+                                                <FaIcons.FaDollarSign />
+                                            </div>
+                                            <span>{job.salaryRange}</span>
+                                        </div>
                                     </div>
                                     <button
-                                        onClick={() => applyJob(job._id)}
-                                        className="w-full py-2 text-white bg-green-600 hover:bg-green-700 rounded-md font-medium transition-colors"
+                                        onClick={() => setSelectedJob(job)}
+                                        className="md:w-1/4 w-1/3 align-right text-xs p-1 font-medium transition-colors hover:text-gray-400 border-[1px] hover:border-gray-400 border-blue-500 text-blue-500 rounded-md"
                                     >
-                                        Apply Now
+                                        Learn More
                                     </button>
                                 </div>
                             </div>
@@ -191,7 +175,7 @@ const Careers = () => {
 
             {/* Just make everything simple , dont use too much popup things , */}
             {/* when You can make just simple card with good layout */}
-            {/* {selectedJob && (
+            {selectedJob && (
                 <div className="fixed inset-0 text-gray-300 dark:text-gray-600 flex items-center justify-center z-10">
                     <div className="bg-[var(--opac)] backdrop-blur-3xl border-[1px] border-[var(--opac)] animate-appear blockanim dark:border-gray-400 md:p-6 p-3 rounded-xl lg:w-[85%] shadow-black/80 dark:shadow-black/40 shadow-2xl w-[94%]">
                         <h2 className="text-xl font-medium mb-2 text-center">
@@ -272,29 +256,68 @@ const Careers = () => {
                                     </p>
                                 </div>
                             </div>
+                            <div className="flex items-center space-x-4">
+                                <div className="w-10 h-10 border-[1px] dark:border-gray-400 flex justify-center items-center rounded-full">
+                                    <FaIcons.FaBuilding />
+                                </div>
+                                <div>
+                                    <p className="font-light text-xs">
+                                        Company
+                                    </p>
+                                    <p className="md:text-lg text-md">
+                                        {selectedJob.company}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex items-center space-x-4">
+                                <div className="w-10 h-10 border-[1px] dark:border-gray-400 flex justify-center items-center rounded-full">
+                                    <FaIcons.FaCalendarAlt />
+                                </div>
+                                <div>
+                                    <p className="font-light text-xs">
+                                        Posting Date
+                                    </p>
+                                    <p className="md:text-lg text-md">
+                                        {selectedJob.postingDate}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex items-center space-x-4">
+                                <div className="w-10 h-10 border-[1px] dark:border-gray-400 flex justify-center items-center rounded-full">
+                                    <FaIcons.FaClock />
+                                </div>
+                                <div>
+                                    <p className="font-light text-xs">
+                                        Application Deadline
+                                    </p>
+                                    <p className="md:text-lg text-md">
+                                        {selectedJob.applicationDeadline}
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="mt-4 p-3 bg-[var(--opac)] dark:bg-gray-100 backdrop-blur-md shadow-black/30 dark:shadow-none shadow-md rounded-md">
+                        {/* <div className="mt-4 p-3 bg-[var(--opac)] dark:bg-gray-100 backdrop-blur-md shadow-black/30 dark:shadow-none shadow-md rounded-md">
                             <p className="text-justify">
                                 {selectedJob.details}
                             </p>
-                        </div>
-                        <div className="flex justify-evenly mt-5">
+                        </div> */}
+                        <div className="flex mt-5">
                             <button
                                 onClick={() => applyJob(selectedJob._id)}
-                                className="text-white bg-[green] hover:bg-gray-300 p-2 rounded-md"
+                                className="text-green-600 border-[1px] border-green-600 hover:border-blue-500 hover:text-blue-500 px-4 p-1 rounded-md"
                             >
-                                Apply Now
+                                Apply
                             </button>
                             <button
                                 onClick={() => setSelectedJob(null)}
-                                className="text-white bg-[red] hover:bg-gray-300 p-2 rounded-md"
+                                className="ml-4 text-red-600 border-[1px] border-red-600 hover:border-blue-500 hover:text-blue-500 px-4 p-1 rounded-md"
                             >
                                 Close
                             </button>
                         </div>
                     </div>
                 </div>
-            )} */}
+            )}
         </div>
     )
 }
