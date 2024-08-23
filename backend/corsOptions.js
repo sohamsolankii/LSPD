cac2e91db75e225d82e70d93d39680a256df8a18
Soham Solanki <sohammsolanki@gmail.com>
2024-08-23 06:09:01 +0530

    cors options changed

--- backend/corsOptions.js
+++ backend/corsOptions.js
@@ -1,11 +1,17 @@
+var whitelist = [
+    'https://lspd-sohams-projects-4d3c8039.vercel.app',
+    'http://localhost:5173',
+]
+
 export const corsOptions = {
     origin: function (origin, callback) {
-        const allowedOrigin = process.env.CORS_ORIGIN
-        if (origin && origin.startsWith(allowedOrigin)) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
-            callback(null, '*')
+            callback(new Error('Not allowed by CORS'))
         }
     },
     credentials: true,
+    methods: '*', // Correct spelling
+    allowedHeaders: '*', // 'headers' key should be 'allowedHeaders'
 }
